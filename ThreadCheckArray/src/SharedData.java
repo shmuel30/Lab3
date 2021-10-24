c6072a247c46a7247ceceb253b98f9a6b77b43ae
Aneel <Aneel@LAPTOP-O8NLUAE5>
2021-10-24 12:18:44 +0300

    change sharedData

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,14 +1,15 @@
 public class SharedData 
 {
-	private int [] array;
+	private ArrayList<Integer> array;
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
 	
-	public SharedData(int[] array, int b) {
+	public SharedData(ArrayList<Integer> array, int b) {
 		
 		this.array = array;
 		this.b = b;
+		
 	}
 
 	public boolean[] getWinArray() 
@@ -21,7 +22,7 @@ public class SharedData
 		this.winArray = winArray;
 	}
 
-	public int[] getArray() 
+	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
